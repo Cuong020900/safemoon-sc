eedf3a9ed95189182e940776a8f43f7ebf382d1a
Haaland-Huyn <huyn@safemoon.ltd>
2022-08-20 16:17:13 +0700

    test: transferFrom unit test

--- test/safemoon.spec.ts
+++ test/safemoon.spec.ts
@@ -10,6 +10,7 @@ describe("Safemoon", () => {
   let wallets: Wallet[];
   let deployer: Wallet;
   let account1: Wallet;
+  let account2: Wallet;
   let safemoon: ISafemoon & Safemoon;
   let oldSafemoon: ISafemoon & OldSafemoon;
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
@@ -19,6 +20,7 @@ describe("Safemoon", () => {
     wallets = await (ethers as any).getSigners();
     deployer = wallets[0];
     account1 = wallets[1];
+    account2 = wallets[2];
   });
 
   beforeEach(async () => {
@@ -101,6 +103,93 @@ describe("Safemoon", () => {
     const deployerSFMBalance01 = await safemoon.balanceOf(deployer.address);
     const deployerOldSFMBalance01 = await oldSafemoon.balanceOf(deployer.address);
 
+    expect(acc1OldSFMBalance01).eq(acc1SFMBalance01);
+    expect(deployerSFMBalance01).eq(deployerOldSFMBalance01);
+
+    const zeroSFMBalance01 = await safemoon.balanceOf(zeroAddr);
+    const zeroOldSFMBalance01 = await oldSafemoon.balanceOf(zeroAddr);
+    expect(zeroSFMBalance01).eq(zeroOldSFMBalance01);
+  });
+
+  it("transferFrom from exclude to non-exclude wallet", async () => {
+    await safemoon.approve(account2.address, ethers.constants.MaxUint256);
+    await oldSafemoon.approve(account2.address, ethers.constants.MaxUint256);
+
+    // add deployer to exclude
+    await safemoon.excludeFromReward(deployer.address);
+    await oldSafemoon.excludeFromReward(deployer.address);
+
+    const acc1SFMBalance = await safemoon.balanceOf(account1.address);
+    const acc1OldSFMBalance = await oldSafemoon.balanceOf(account1.address);
+    const zeroSFMBalance = await safemoon.balanceOf(zeroAddr);
+    const zeroOldSFMBalance = await oldSafemoon.balanceOf(zeroAddr);
+    expect(zeroSFMBalance).eq(zeroOldSFMBalance);
+    expect(acc1SFMBalance).eq(0);
+    expect(acc1OldSFMBalance).eq(0);
+
+    // est gas limit
+    const gasLimit = await safemoon
+      .connect(account2)
+      .estimateGas.transferFrom(deployer.address, account1.address, parseEther("10"));
+    const oldGasLimit = await oldSafemoon
+      .connect(account2)
+      .estimateGas.transferFrom(deployer.address, account1.address, parseEther("10"));
+    console.log("\x1b[36m%s\x1b[0m", "gasLimit", gasLimit.toString());
+    console.log("\x1b[36m%s\x1b[0m", "oldGasLimit", oldGasLimit.toString());
+
+    await safemoon.connect(account2).transferFrom(deployer.address, account1.address, parseEther("10"));
+    await oldSafemoon.connect(account2).transferFrom(deployer.address, account1.address, parseEther("10"));
+    const acc1SFMBalance01 = await safemoon.balanceOf(account1.address);
+    const acc1OldSFMBalance01 = await oldSafemoon.balanceOf(account1.address);
+    const deployerSFMBalance01 = await safemoon.balanceOf(deployer.address);
+    const deployerOldSFMBalance01 = await oldSafemoon.balanceOf(deployer.address);
+    const acc2SFMBalance01 = await safemoon.balanceOf(deployer.address);
+    const acc2OldSFMBalance01 = await oldSafemoon.balanceOf(deployer.address);
+
+    expect(acc2SFMBalance01).eq(acc2OldSFMBalance01);
+    expect(acc1OldSFMBalance01).eq(acc1SFMBalance01);
+    expect(deployerSFMBalance01).eq(deployerOldSFMBalance01);
+
+    const zeroSFMBalance01 = await safemoon.balanceOf(zeroAddr);
+    const zeroOldSFMBalance01 = await oldSafemoon.balanceOf(zeroAddr);
+    expect(zeroSFMBalance01).eq(zeroOldSFMBalance01);
+  });
+
+  it("transferFrom from non-exclude to exclude wallet", async () => {
+    await safemoon.approve(account2.address, ethers.constants.MaxUint256);
+    await oldSafemoon.approve(account2.address, ethers.constants.MaxUint256);
+    // add deployer to exclude
+    await safemoon.excludeFromReward(account1.address);
+    await oldSafemoon.excludeFromReward(account1.address);
+
+    const acc1SFMBalance = await safemoon.balanceOf(account1.address);
+    const acc1OldSFMBalance = await oldSafemoon.balanceOf(account1.address);
+    const zeroSFMBalance = await safemoon.balanceOf(zeroAddr);
+    const zeroOldSFMBalance = await oldSafemoon.balanceOf(zeroAddr);
+    expect(zeroSFMBalance).eq(zeroOldSFMBalance);
+    expect(acc1SFMBalance).eq(0);
+    expect(acc1OldSFMBalance).eq(0);
+
+    // est gas limit
+    const gasLimit = await safemoon
+      .connect(account2)
+      .estimateGas.transferFrom(deployer.address, account1.address, parseEther("10"));
+    const oldGasLimit = await oldSafemoon
+      .connect(account2)
+      .estimateGas.transferFrom(deployer.address, account1.address, parseEther("10"));
+    console.log("\x1b[36m%s\x1b[0m", "gasLimit", gasLimit.toString());
+    console.log("\x1b[36m%s\x1b[0m", "oldGasLimit", oldGasLimit.toString());
+
+    await safemoon.connect(account2).transferFrom(deployer.address, account1.address, parseEther("10"));
+    await oldSafemoon.connect(account2).transferFrom(deployer.address, account1.address, parseEther("10"));
+    const acc1SFMBalance01 = await safemoon.balanceOf(account1.address);
+    const acc1OldSFMBalance01 = await oldSafemoon.balanceOf(account1.address);
+    const deployerSFMBalance01 = await safemoon.balanceOf(deployer.address);
+    const deployerOldSFMBalance01 = await oldSafemoon.balanceOf(deployer.address);
+    const acc2SFMBalance01 = await safemoon.balanceOf(deployer.address);
+    const acc2OldSFMBalance01 = await oldSafemoon.balanceOf(deployer.address);
+
+    expect(acc2SFMBalance01).eq(acc2OldSFMBalance01);
     expect(acc1OldSFMBalance01).eq(acc1SFMBalance01);
     expect(deployerSFMBalance01).eq(deployerOldSFMBalance01);
 
